a9583e20805829a1486b422a833a2d6ebd7d6dfb
sachinzemoso <sachin.goyal@zemosolabs.com>
2024-01-24 18:21:48 +0530

    feat: async image loading and closure

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,6 +2,96 @@ const submitContactForm = () => {
     alert('Thankyou  for submitting the form');
 }
 
+
+/**
+ * JS to load images asynchronously 
+ * images : images data
+ * loadImageAsync : function to load images asynchronously using Promise
+ * appendImageToContainer : Function to append images to the div
+ * loadImages : Load images asynchronously
+ */
+
+const images = [
+    { url: 'assets/image/gallery/1.jpg', alt: "Image of me on roof top." },
+    { url: 'assets/image/gallery/2.jpg', alt: "Image of me on beach." },
+    { url: 'assets/image/gallery/3.jpg', alt: "Image of me hill climbing." },
+    { url: 'assets/image/gallery/4.jpg', alt: "Image of me in shallow river." },
+    { url: 'assets/image/gallery/5.jpg', alt: "Image of me on a ferry top." }
+];
+
+async function loadImages() {
+    try {
+        for (const image of images) {
+            const img = await loadImageAsync(image);
+            appendImageToContainer(img);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function loadImageAsync(image) {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = image.url;
+        img.alt = image.alt;
+        img.onload = () => resolve(img);
+        img.onerror = () => reject(new Error(`Failed to load image from ${url}`));
+    });
+}
+
+
+function appendImageToContainer(img) {
+    const galleryDiv = document.getElementById('galleryDiv')
+    if (galleryDiv) {
+        galleryDiv.appendChild(img);
+    }
+}
+
+if (window.location.pathname === '/gallery.html') {
+    loadImages();
+}
+
+/**
+ * Closure for screen size
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+    function makeSizer(size) {
+        return function () {
+            document.querySelector('h1').style.fontSize = `${size}px`;
+            document.querySelectorAll('main h2').forEach(element => {
+                element.style.fontSize = `${size - 5}px`;
+            });
+            document.querySelectorAll('h3').forEach(element => {
+                element.style.fontSize = `${size - 10}px`;
+            });
+            document.querySelectorAll('h4').forEach(element => {
+                element.style.fontSize = `${size - 15}px`;
+            });
+            document.querySelectorAll('p').forEach(element => {
+                element.style.fontSize = `${size - 20}px`;
+            });
+            document.querySelectorAll('ol li').forEach(element => {
+                element.style.fontSize = `${size - 20}px`;
+            });
+            document.querySelectorAll('td,th').forEach(element => {
+                element.style.fontSize = `${size - 20}px`;
+            });
+        };
+    }
+
+    const small = makeSizer(30);
+    const medium = makeSizer(40);
+    const large = makeSizer(50);
+
+
+    document.getElementById('size-s').onclick = small;
+    document.getElementById('size-m').onclick = medium;
+    document.getElementById('size-l').onclick = large;
+
+});
+
 /**
  * JS Assigment - 1
  * Demonstrate to pass function as argument to another function
